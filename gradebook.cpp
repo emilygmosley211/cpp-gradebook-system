4b4af629204d23bc59f358768fa626ded20cf0d7
emilygmosley211 <mosley.e078056@stu.sanjac.edu>
2025-04-24 18:15:33 +0000

    Adding statistics function to calculate average, highest, and lowest grades. Input main program as well which prompts each user to enter number of students and their name and grades.

--- gradebook.cpp
+++ gradebook.cpp
@@ -23,3 +23,33 @@ int searchStudent(string names[], int grades[], int count, string target) {
     }
     return -1;
 }
+
+void calculateStatistics(int grades[], int count, double &average, int &highest, int &lowest) {
+    int sum = 0;
+    highest = grades[0];
+    lowest = grades[0];
+    for (int i = 0; i < count; ++i) {
+        sum += grades[i];
+        if (grades[i] > highest) highest = grades[i];
+        if (grades[i] < lowest) lowest = grades[i];
+    }
+    average = static_cast<double>(sum) / count;
+}
+
+int main() {
+    int numStudents;
+    string studentNames[MAX_STUDENTS];
+    int studentGrades[MAX_STUDENTS];
+
+    cout << "Enter the number of students: ";
+    cin >> numStudents;
+
+    for (int i = 0; i < numStudents; ++i) {
+        cout << "Enter student name: ";
+        cin >> ws;
+        getline(cin, studentNames[i]);
+        cout << "Enter " << studentNames[i] << "'s grade: ";
+        cin >> studentGrades[i];
+    }
+
+    sortGrades(studentNames, studentGrades, numStudents);
